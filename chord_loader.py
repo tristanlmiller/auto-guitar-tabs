3afbf1401ebcf34c1f8c05f29234c4e0c7615295
Tristan Miller <tristanmiller@Tristans-MacBook-Pro.local>
2019-09-18 22:48:36 -0700

    further debugging of chord_loader

--- chord_loader.py
+++ chord_loader.py
@@ -24,24 +24,39 @@ shorthand_to_quality = {'1':'unison',
                         'min9':'min','maj9':'maj','9':'maj',
                         '11':'maj','min11':'min','13':'maj','maj13':'maj','min13':'min'}
 
+#chord quality -> number class
+quality_to_num = {'unison':0,'power':1,'maj':2,'min':3,'sus':4,'dim':5,'aug':6}
+#number class -> chord quality
+num_to_quality = {0:'unison',1:'power',2:'maj',3:'min',4:'sus',5:'dim',6:'aug'}
+
 #shorthand -> added interval, standardized
 shorthand_to_add = {'1':'',
                     '5':'',
                     'maj':'','min':'','dim':'','aug':'','sus4':'',#'sus2':'',
-                    '7':'min7','min7':'min7','maj7':'maj7','hdim7':'min7','dim7':'6','minmaj7':'maj7',
+                    '7':'7','min7':'7','maj7':'maj7','hdim7':'7','dim7':'6','minmaj7':'maj7',
                     'maj6':'6','min6':'6',
                     'min9':'9','maj9':'9','9':'9',
                     '11':'4','min11':'4','13':'6','maj13':'6','min13':'6'}
 
 #interval list -> added interval, standardized
 interval_to_add = {'7':'maj7',
-                   'b7':'min7','#6':'min7',
+                   'b7':'7','#6':'7',
                    '9':'9','2':'9',
                    'b9':'min9','b2':'min9',
                     '11':'4','4':'4',
                   '#11':'tt','#4':'tt','b5':'tt',
                   '13':'6','6':'6',
                    'b13':'min6','b6':'min6','#5':'min6'}
+
+#added interval -> number class
+add_to_num = {'':0,'maj7':1,'7':2,'9':3,'min9':4,'4':5,'tt':6,'6':7,'min6':8}
+#number class -> added interval, standardized
+num_to_add = {0:'',1:'maj7',2:'7',3:'add 9',4:'add m9',5:'add 4',6:'add tt',7:'add 6',8:'add m6'}
+
+#inversion -> number class
+inv_to_num = {'':0,'5':1,'3':2}
+#number class -> inversion, standardized
+num_to_inv = {0:'',1:'/5',2:'/3'}
 
 ############################################################
 #Functions used in simplifying chord properties
@@ -54,45 +69,52 @@ def get_root(row):
     else:
         return root_to_num[row[0]]
 
+#returns quality in int format
 def get_quality(row):
     if row[1] in shorthand_to_quality:
-        return shorthand_to_quality[row[1]]
+        return quality_to_num[shorthand_to_quality[row[1]]]
     elif (row[0] != 'N') and (row[0] != 'X'):
         #some chords just aren't labeled properly, but I think they're major chords.
         if row[2] == '1,5':
-            return 'power'
+            return quality_to_num['power']
         elif row[2] == '1':
-            return 'unison'
+            return quality_to_num['unison']
         else:
-            return 'maj'
+            return quality_to_num['maj']
     else:
         return np.nan
 
-#return inversion type, ignoring any past 5/3
+#return inversion type in int format.  Only counts first and second inversions
 def simplify_inversion(row):
     if row[1] == 'sus2':
-        return np.nan
+        return inv_to_num['']
     if row[3] in ['5','b5','#5','#4']:
-        return '5'
+        return inv_to_num['5']
     if row[3] in ['3','b3','b4']:
-        return '3'
+        return inv_to_num['3']
     else:
-        return np.nan
+        return inv_to_num['']
 
-#return added interval
+#return added interval in int format
 def get_add(row):
     if row[1] not in shorthand_to_add:
+        #if there's no valid shorthand (i.e. N or X chord)
         return np.nan
     out = shorthand_to_add[row[1]]
     if out:
-        return out
-    if not row[2]:
-        return np.nan
+        #if the shorthand implies an add
+        return add_to_num[out]
+    elif not row[2]:
+        #if shorthand does not imply add, and none is available
+        return add_to_num['']
+    #if shorthand implies no add, but an add is given
     interval = str(row[2]).split(',')[-1]
     if interval not in interval_to_add:
-        return np.nan
+        #if the given add isn't one of the ones I care about (e.g. 5)
+        return add_to_num['']
     else:
-        return interval_to_add[interval]
+        #if the given add is one I care about
+        return add_to_num[interval_to_add[interval]]
 
 ############################################################
 #Functions for loading and processing files
@@ -107,11 +129,11 @@ def get_features_labels(song_list,block_length,minfreq,num_octaves,bins_per_note
     minfreq - minimum frequency analyzed
     num_octaves - number octaves analyzed
     bins_per_note - number of bins per note (12 notes per octave)
-    transposition - True if you want to duplicate and transpose data"""
+    transpose - True if you want to duplicate and transpose data"""
     block_length = get_true_block(block_length,minfreq,num_octaves,bins_per_note)
     
     all_features = np.ndarray((0,(12*num_octaves+11)*bins_per_note+1))
-    all_labels = pd.DataFrame()
+    all_labels = np.ndarray((0,4))
     for i, row in song_list.iterrows():
         if row.dataset == 'isophonics/The Beatles':
             sep = ' '
@@ -119,54 +141,80 @@ def get_features_labels(song_list,block_length,minfreq,num_octaves,bins_per_note
             sep = '\t'
         song_labels = get_labels(row.chord_filepath,sep,block_length)
         song_features = get_features(row.mp3_filepath,block_length,minfreq,num_octaves,bins_per_note)
-        #note: song_labels is a DataFrame, song_features is an ndarray
+        #note: both of these are ndarrays
         
         #remove rows that don't appear in both arrays
         if song_labels.shape[0] > song_features.shape[0]:
-            song_labels = song_labels.iloc[:(song_features.shape[0]),:]
+            song_labels = song_labels[:(song_features.shape[0]),:]
         elif song_labels.shape[0] < song_features.shape[0]:
             song_features = song_features[:(song_labels.shape[0]),:]
-        good_rows = ~np.isnan(song_labels['root'])
-        song_labels = song_labels.loc[good_rows,:]
+        good_rows = ~np.isnan(song_labels[:,0])
+        song_labels = song_labels[good_rows,:]
         song_features = song_features[good_rows,:]
         
         all_features = np.concatenate((all_features,song_features),axis=0)
-        all_labels = all_labels.append(song_labels,ignore_index=True)
+        all_labels = np.concatenate((all_labels,song_labels),axis=0)
     
     if transpose:
         og_size = all_features.shape[0]
         num_features = all_features.shape[1]
-        num_labels = all_labels.shape[1]
         labels_copy = all_labels.copy()
-        transposition = np.zeros((og_size*12,))
-        t_counter = 0
         #duplicate and transpose data
         for i in range(-5,0):
             duplicate_features = np.zeros((og_size,num_features))
-            duplicate_features[:,:i] = all_features[:og_size,(-i):]
+            duplicate_features[:,1:(i*bins_per_note)] = all_features[:og_size,(1-i*bins_per_note):]
+            duplicate_features[:,0] = all_features[:og_size,0]
             duplicate_labels = labels_copy.copy()
-            duplicate_labels['root'] = duplicate_labels['root'].apply(lambda x: transpose_root(x,i))
+            duplicate_labels[:,0] = transpose_root(duplicate_labels[:,0],i)
             
             all_features = np.concatenate((all_features,duplicate_features),axis=0)
-            all_labels = all_labels.append(duplicate_labels,ignore_index=True)
-            t_counter += 1
-            transposition[(og_size*t_counter):(og_size*(t_counter+1))] = i
+            all_labels = np.concatenate((all_labels,duplicate_labels),axis=0)
             
         for i in range(1,7):
             duplicate_features = np.zeros((og_size,num_features))
-            duplicate_features[:,i:] = all_features[:og_size,:(-i)]
+            duplicate_features[:,(1+i*bins_per_note):] = all_features[:og_size,1:(-i*bins_per_note)]
+            duplicate_features[:,0] = all_features[:og_size,0]
             duplicate_labels = labels_copy.copy()
-            duplicate_labels['root'] = duplicate_labels['root'].apply(lambda x: transpose_root(x,i))
+            duplicate_labels[:,0] = transpose_root(duplicate_labels[:,0],i)
             
             all_features = np.concatenate((all_features,duplicate_features),axis=0)
-            all_labels = all_labels.append(duplicate_labels,ignore_index=True)
-            t_counter += 1
-            transposition[(og_size*t_counter):(og_size*(t_counter+1))] = i
+            all_labels = np.concatenate((all_labels,duplicate_labels),axis=0)
         
-        return all_features,all_labels,transposition
+    return all_features,all_labels
+
+def standardize_root(features,labels,bins_per_note,dropna=True,transposed=False):
+    """Given a set of features and labels, transposes each time block so that root is 0
+    if dropna is true, then rows with N or X chords are dropped
+    if transposed is true, then only takes first 1/12th of the data (the rest being transposed)"""
+    if transposed:
+        og_size = int(features.shape[0]/12)
     else:
-        return all_features,all_labels
+        og_size = features.shape[0]
+    num_features = features.shape[1]
     
+    duplicate_features = np.zeros((og_size,num_features))
+    duplicate_features[:,0] = features[:og_size,0]
+    for j,i in enumerate(labels[:og_size,0]):
+        i = -int(i)
+        if i > 0 and i <= 6:
+            duplicate_features[j,(1+i*bins_per_note):] = features[j,1:(-i*bins_per_note)]
+        elif i > 6:
+            i -= 12
+            duplicate_features[j,1:(i*bins_per_note)] = features[j,(1-i*bins_per_note):]
+        elif i == 0:
+            duplicate_features[j,:] = features[j,:]
+    if dropna:
+        valid_rows = np.logical_and(~np.equal(labels[:og_size,0], -1), ~np.isnan(labels[:og_size,0]))
+        duplicate_features = duplicate_features[valid_rows,:]
+        duplicate_labels = (labels[:og_size,:])[valid_rows,:]
+    return duplicate_features, duplicate_labels
+                                                  
+                                                  
+def transpose_root(roots,transposition):
+    """transposes an array of roots"""
+    roots[roots >= 0] = (roots[roots >= 0] + transposition) % 12
+    return roots
+
 def get_labels(filepath,sep,block_length):
     """loads and processes chord data from target file"""
     df = pd.read_csv(filepath,sep=sep,header=None,names=["start_time","end_time","chord"])
@@ -227,32 +275,25 @@ def blockify(df,block_length):
     output[0] = 'N'
     return output
 
-def transpose_root(rootnum,transposition):
-    """transposes the root (in number format, e.g. 0=C, 1=Db)"""
-    if rootnum < 0:
-        return rootnum
-    else:
-        return (rootnum + transposition) % 12
-
 def chord_simplify(chords):
     '''
     Interprets and simplifies the chords, given a pd.Series of chords.
-    creates a Dataframe with four colums: root, quality, add, and inversion.
+    creates a ndarray with four colums: root, quality, add, and inversion.
     '''
     #extract string matches from chord column
     matchstr = r'^([^:(\/]+):?([^(\/]+)?(?:\(([^)]+)\))?(?:\/(\S*))?$'
     m = chords.str.extract(matchstr)
     #m[0] is the root
-    #m[1] is the shorthand (e.g. min7, maj
+    #m[1] is the shorthand (e.g. min7, maj)
     #m[2] is added intervals
     #m[3] is bass
     
-    df = pd.DataFrame()
-    df['root'] = m.apply(lambda x: get_root(x),axis=1)
-    df['quality'] = m.apply(lambda x: get_quality(x),axis=1)
-    df['add'] = m.apply(lambda x: get_add(x),axis=1)
-    df['inversion'] = m.apply(lambda x: simplify_inversion(x),axis=1)
+    out = np.zeros((m.shape[0],4))
+    out[:,0] = m.apply(lambda x: get_root(x),axis=1)
+    out[:,1] = m.apply(lambda x: get_quality(x),axis=1)
+    out[:,2] = m.apply(lambda x: get_add(x),axis=1)
+    out[:,3] = m.apply(lambda x: simplify_inversion(x),axis=1)
     
-    return df
+    return out
 
 
