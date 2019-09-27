283299807d4b579ba99e98cdbda3a57b6a516b02
Tristan Miller <tristanmiller@Tristans-MacBook-Pro.local>
2019-09-26 22:54:03 -0700

    change model used by app

--- flaskapp/views.py
+++ flaskapp/views.py
@@ -33,7 +33,7 @@ def chord_output():
     url = request.args.get('youtube_url')
     video_code = url[32:]
     
-    model_path = 'xgb_partial'
+    model_path = 'xgb_final'
     lr_predict.predict(url, model_path, f"{video_code}", 0.5, 24.5, 7, 1,
                         'Models/', 'Results/')
 
